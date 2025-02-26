bab6fc55045f28afec492c4c8e5a09eb11dc5a7d
h12z <private@h12z.me>
2025-02-26 18:09:19 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use crate::minecraft_server::{write_unsigned_short, write_var_int, MinecraftServer, Packet};
+
+mod minecraft_server;
+mod auth;
+
+fn main() {
+    let mut minecraft_server = MinecraftServer::new("192.168.178.135:25565".parse().unwrap());
+
+    let mut handshake_request_buffer = Vec::new();
+    write_var_int(&mut handshake_request_buffer, 47);
+    let server_address = "192.168.178.135";
+    write_var_int(&mut handshake_request_buffer, server_address.len() as i32);
+    handshake_request_buffer.extend_from_slice(server_address.as_bytes());
+    write_unsigned_short(&mut handshake_request_buffer, 25565);
+    write_var_int(&mut handshake_request_buffer, 2);
+    minecraft_server.send_packet(&Packet { id: 0x00, data: handshake_request_buffer });
+
+
+}
